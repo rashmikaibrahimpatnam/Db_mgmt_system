a1aab72f5784daae42800aa0d9348125b6a66e43
Yash <ys432526@dal.ca>
2020-12-02 05:08:45 +0530

    release

--- database/db5408_SQLDUMP.sql
+++
@@ -1,3 +0,0 @@
-create table player (player_id int not null auto_increment,team_id int default null,league_id int not null,player_name varchar(45) not null,position varchar(45) not null,age int not null,primary key (player_id),foreign key (team_id) references team (team_id),foreign key (league_id) references league (league_id));
-create table league (league_id int not null auto_increment,league_name varchar(45) not null,primary key (league_id));
-create table team (team_id int not null auto_increment,team_name int default null,league_id int not null,primary key (team_id),foreign key (league_id) references league (league_id));
